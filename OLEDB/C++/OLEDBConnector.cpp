5492edc56f1d1a3347c8495b64089d93f31690e3
SnowFleur <SnowFleur0128@gmail.com>
2022-07-14 00:42:13 +0900

    OLEDB SQL Server Connect 확인

--- OLEDB/C++/OLEDBConnector.cpp
+++ OLEDB/C++/OLEDBConnector.cpp
@@ -31,83 +31,67 @@ CDataSource ds;
 CSession	session;
 CCommand<CAccessor<CState>> cust;
 
-
 /*
 To do(순서)
-- MSSQL 연동 테스트
-- Raw Query 테스트
+- MSSQL 연동 테스트		--> OK
+- Raw Query 테스트		--> Curr
 - SP 테스트
 */
+
+namespace SnowOELDB{}
+
+namespace SnowRedis {}
+
+
+
+bool SQLServerConnect( const WCHAR* pDataBase, const WCHAR* pSQLServer, const WCHAR* pAuthUserID, const WCHAR* pAuthUserPW )
+{
+	HRESULT hr = CoInitialize(0);
+
+	WCHAR arrSqlCoonect[1024];
+	ZeroMemory(arrSqlCoonect, sizeof(arrSqlCoonect));
+
+	CDBPropSet	dbinit(DBPROPSET_DBINIT);
+
+	//SQL Server
+	if (dbinit.AddProperty(DBPROP_INIT_DATASOURCE, pSQLServer) == false) return false;
+
+	//DB
+	if (dbinit.AddProperty(DBPROP_INIT_CATALOG, pDataBase) == false) return false;
+
+	//AuthUSerID
+	if (dbinit.AddProperty(DBPROP_AUTH_USERID, pAuthUserID) == false) return false;
+
+	//AuthUserPW
+	if (dbinit.AddProperty(DBPROP_AUTH_PASSWORD, pAuthUserPW) == false) return false;
+
+
+	//SQL Server 연결시도
+	hr = ds.Open(L"SQLOLEDB", &dbinit);
+
+	if (FAILED(hr))
+	{
+		std::cout << "SQL Server와 연결을 실패했습니다\n";
+		return false;
+	}
+
+	//세션을 시작
+	hr = session.Open(ds);
+
+	if (FAILED(hr)) 
+	{
+		std::cout << "session.Open 에러\n";
+		return false;
+	}
+
+
+	std::cout << "SQL Server Connect!\n";
+	return true;
+}
 
 int main()
 {
 
-	try
-	{
-		HRESULT hr = CoInitialize(0);
-
-		if (FAILED(hr)) 
-		{
-			std::cout << " COM 초기화 할 수 없습니다.\n";
-			return -1;
-		}
-
-		//데이터베이스 연결
-		CDBPropSet	dbinit(DBPROPSET_DBINIT);
-		dbinit.AddProperty(DBPROP_INIT_PROMPT, (SHORT)4);
-		dbinit.AddProperty(DBPROP_INIT_PROVIDERSTRING, OLESTR("PROVIDER=SQLOLEDB.1;SRVER아이피주소 어쩌구 패스워드 DB이름)"));
-
-		dbinit.AddProperty(DBPROP_INIT_LCID,(LONG)1043);	//Local Identifer;
-
-		hr = ds.Open(_T("SQLOLEDB"), &dbinit);
-
-		if (FAILED(hr))
-		{
-			std::cout << "데이터 베이스를 열 수 없습니다.\n";
-			return -1;
-		}
-
-		//세션을 시작
-		hr = session.Open(ds);
-		
-		if (FAILED(hr)) 
-		{
-			std::cout << "SESSION을 시작할 수 없습니다.\n";
-			ds.Close();
-			return -1;
-		}
-
-		//사용할 쿼리문을 문자열로 만들어 둠
-		char mySQL[] = "SELECT * FROM state";
-
-		//쿼리문을 실행한다.
-		hr = cust.Open(session, mySQL);
-
-		if (FAILED(hr)) 
-		{
-			std::cout << "쿼리문을 실행한다.\n";
-			session.Close();
-			ds.Close();
-			return -1;
-		}
-
-		//읽어온 데이터를 화면에 출력한다.
-		while (cust.MoveNext()==S_OK)
-		{
-			std::cout << cust.ip << ", " << cust.data << " " << cust.time << "\n";
-		}
-		cust.Close();
-		session.Close();
-		ds.Close();
-
-		return 1;
-
-	}
-	catch (...)
-	{
-		std::cout << "Error 발생\n";
-		return -1;
-	}
 }
 
 
